2af725d0a5db28fc82a7257bfb91e1b53e44e252
TechGenius1234 <evan.degroot.network@outlook.com>
2024-07-02 13:27:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-var users = {
-    "admin": "password",
-    "user1": "pass123",
-    "user2": "test456"
-};
-
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     
-    if (users.hasOwnProperty(username) && users[username] === password) {
-        switch (username) {
-            case 'admin':
-                window.location.href = 'https://github.com/';
-                break;
-            case 'user1':
-                window.location.href = 'https://github.com/user1';
-                break;
-            case 'user2':
-                window.location.href = 'https://github.com/user2';
-                break;
-            default:
-                break;
-        }
+    // Simple validation
+    if (username === 'evan.degroot' && password === '20459$78') {
+        window.location.href = 'https://github.com/';
     } else {
         document.getElementById('message').textContent = 'Invalid username or password!';
     }
